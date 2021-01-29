c268b4382a388a7a80d0cd0087c26900527bbf56
Adnan Afsari <adnanafsari.swe@gmail.com>
2021-01-29 10:59:35 +0600

    Pencil positined

--- src/components/banner.js
+++ src/components/banner.js
@@ -21,6 +21,31 @@ const Banner = () => (
 			}}>
 			<img style={{borderRadius: '9990px'}} src={t1} alt="t1" />
 		</div>
+		<div
+			style={{
+				position: 'absolute',
+				top: '10.1rem'
+			}}>
+			<img style={{}} src={pencil} alt="t1" />
+		</div>
+		{/* <div
+			style={{
+				position: 'absolute'
+			}}>
+			<img style={{}} src={ball} alt="t1" />
+		</div>
+		<div
+			style={{
+				position: 'absolute'
+			}}>
+			<img style={{ borderRadius: '9990px' }} src={ruler} alt="t1" />
+		</div>
+		<div
+			style={{
+				position: 'absolute'
+			}}>
+			<img style={{ borderRadius: '9990px' }} src={trophy} alt="t1" />
+		</div> */}
 	</section>
 )
 
